9b5a20372a10129b82ae6b495fb46f9f63146ed5
Are Kristoffer Lamo <arelamo@studioare.net>
2020-12-07 23:17:35 +0100

    Add CORS header

--- backend/server.py
+++ backend/server.py
@@ -3,6 +3,7 @@ import asyncio
 import os
 from aiohttp import web
 from GitHubAPI import GitHubAPI
+import aiohttp_cors
 
 routes = web.RouteTableDef()
 
@@ -16,7 +17,14 @@ async def hello(request):
 async def handle(request):
     name = request.match_info.get('name')
     repos = await api.filteredOwned(name)
-    return web.json_response(repos)
+    return web.json_response(repos, status=200)
+
+@routes.get('/relatedRepositories/{name}/{repo}')
+async def details(request):
+    name = request.match_info.get('name')
+    repoName = request.match_info.get('repo')
+    repo = await api.filteredRepo(name, repoName)
+    return web.json_response(repo, status=200)
 
 #https://docs.aiohttp.org/en/stable/deployment.html
 parser = argparse.ArgumentParser(description="aiohttp server test")
@@ -28,6 +36,17 @@ if __name__ == '__main__':
     
     app = web.Application()
     app.add_routes(routes)
+
+    cors = aiohttp_cors.setup(app, defaults={
+        "*": aiohttp_cors.ResourceOptions(
+                expose_headers="*",
+                allow_headers="*",
+        )
+    })
+
+    # Configure CORS on all routes.
+    for route in list(app.router.routes()):
+        cors.add(route)
     #web.run_app(app)
     args = parser.parse_args()
     web.run_app(app, port=args.port)
